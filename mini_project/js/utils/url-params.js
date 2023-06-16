6cd7bed54bc3e905d548639d2caba67eb6df5bf8
ArtiomDumchykov <66011687+ArtiomDumchykov@users.noreply.github.com>
2023-06-16 17:24:52 +0300

    fix

--- mini_project/js/utils/url-params.js
+++
@@ -1,9 +0,0 @@
-export class URLParams {
-    constructor(location) {
-        this.location = new URL(location)
-    }
-
-    getParam(key) {
-        return this.location.searchParams.get(key)
-    }
-}
